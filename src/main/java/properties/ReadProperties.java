9f10dd9e7648e7f9b4c34db7ed44e0234c8e5126
Anna <giannagrale@gmail.com>
2021-11-22 09:56:31 +0300

    Make ReadProperties a singleton

--- src/main/java/properties/ReadProperties.java
+++ src/main/java/properties/ReadProperties.java
@@ -1,30 +1,44 @@
 
 package properties;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.io.*;
 import java.util.Properties;
 
-public class ReadProperties {
 
+public final class ReadProperties {
+
+    private static final Logger logger = LogManager.getLogger();
     private final Properties properties = new Properties();
+    private static ReadProperties instance;
 
-    public ReadProperties() {
+    public static ReadProperties getInstance() {
+        if (instance == null) {
+            instance = new ReadProperties();
+        }
+        return instance;
+    }
+
+    private ReadProperties() {
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties")) {
             properties.load(inputStream);
         } catch (IOException e) {
+            logger.debug("The file cannot be read...");
             e.printStackTrace();
         }
     }
 
     /***
      * Receives the key value from config.properties
-     * @param key
+     * @param key key from config.properties
      * @return key value
      */
-    public String getPropertyValueByKey(String key) {
+    public String getKeyProperty(String key) {
         if (properties.getProperty(key) != null) {
             return properties.getProperty(key);
         } else {
+            logger.debug("The key value isn't found...");
             throw new RuntimeException();
         }
     }
