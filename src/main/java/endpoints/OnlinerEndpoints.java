dd1db7b3e81c1c93e146b9bb38128cfe17198f68
Anna <giannagrale@gmail.com>
2021-11-22 10:01:56 +0300

    Add endpoints

---
+++ src/main/java/endpoints/OnlinerEndpoints.java
@@ -0,0 +1,8 @@
+package endpoints;
+
+public class OnlinerEndpoints {
+    public static final String appleFilterEndpoint = "mobile?mfr%5B0%5D=apple";
+    public static final String mobileEndpoint = "mobile";
+    public static final String dominosEndpoint = "dominos";
+    public static final String pepperoniEndpoint = "dominos/dominospizza/domipepperonito5";
+}
