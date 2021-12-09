1f3ea910bc1cdabcd2a3d4594805336a7275a95e
Hanna.Hrabouskaya <giannagrale@gmail.com>
2021-12-09 10:34:46 +0300

    Perform property refactor

--- src/main/java/testData/Catalogue.java
+++ src/main/java/testData/Catalogue.java
@@ -4,6 +4,6 @@ package testData;
 public class Catalogue {
 
     public static final String PRICE = "-60";
-    public static final String BRAND = "apple";
+    public static final String APPLE = "apple";
     public static final String PIZZA = "Пицца Domino's Пепперони (классика, 36 см";
 }
