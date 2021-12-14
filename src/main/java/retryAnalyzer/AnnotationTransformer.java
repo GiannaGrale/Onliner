deba293f610208be38883a25aa0bac352f93800d
Hanna.Hrabouskaya <giannagrale@gmail.com>
2021-12-14 15:06:51 +0300

    Add IAnnotationTransformer and Retry for all the tests

---
+++ src/main/java/retryAnalyzer/AnnotationTransformer.java
@@ -0,0 +1,20 @@
+package retryAnalyzer;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+/***
+ * IAnnotationTransformer is a TestNG listener which allows you to modify TestNG annotations and configure them further during runtime.
+ */
+public class AnnotationTransformer implements IAnnotationTransformer {
+
+    /***
+     * Transform method is called for every test during the test run.
+     */
+    @Override
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+        annotation.setRetryAnalyzer(Retry.class);
+    }
+}
