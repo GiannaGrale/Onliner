57184e1a18c1b54d3cf45e7909c86d882b4e574b
Hanna.Hrabouskaya <giannagrale@gmail.com>
2022-01-05 11:34:44 +0300

    Add corrected BrowserConfig

--- src/main/java/drivers/BrowserConfig.java
+++ src/main/java/drivers/BrowserConfig.java
@@ -1,12 +1,22 @@
 package drivers;
 
+import java.util.Arrays;
+
+/**
+ * Get the type of browser to use.
+ */
 public class BrowserConfig {
 
-    /**
-     * Get the type of browser to use.
-     * @return the type of browser
-     */
     public static BrowserType getType() {
-        return BrowserType.valueOf(System.getProperty("browser").toUpperCase());
+        String defaultBrowser = System.getProperty("browser");
+        String browser = defaultBrowser == null ? System.getProperty("browser") : defaultBrowser;
+        return readValue(browser);
+    }
+
+    public static BrowserType readValue(String text) {
+        return Arrays.stream(BrowserType.values())
+                .filter(browser -> browser.name().equalsIgnoreCase(text))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown browser value : " + text));
     }
 }
