e05586bb3bc078378d78f70c83c9b9421d3a2de1
Anna <giannagrale@gmail.com>
2022-01-22 16:34:46 +0300

    Fixes to pom

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -11,7 +11,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import properties.EnvironmentConfig;
-import properties.TestDataStorage;
 import static drivers.DriverManager.getDriver;
 
 @Listeners({OnlinerTestListener.class, UniversalVideoListener.class})
